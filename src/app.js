95f127097ab27279db6817f2c3054f4ec005618e
Ashley Manese <ashleymanese31@gmail.com>
2023-02-05 15:00:00 -0800

    adds get and post request using hard coded data

--- src/app.js
+++ src/app.js
@@ -1,3 +1,42 @@
-const { v4: uuidv4 } = require("uuid");
+const express = require('express');
+const app = express();
 
-console.log(uuidv4());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const PORT = 3000;
+
+const customers = [
+    {
+        "name": "Ashley",
+        "industry": "music"
+    },
+    {
+        "name": "John",
+        "industry": "networking"
+    },
+    {
+        "name": "Sal",
+        "industry": "sports medicine"
+    }
+]
+
+app.get("/", (req,res) => {
+    res.send("hello home page")
+});
+
+app.get('/api/customers', (req, res) => {
+    res.send({"customers" : customers})
+});
+
+app.post('/', (req, res) => {
+    res.send("this is a post request")
+});
+
+app.post('/api/customers', (req,res) => {
+    console.log(req.body)
+    res.send(req.body)
+})
+app.listen(PORT, () => {
+    console.log('App listening on port ' +  PORT)
+});
